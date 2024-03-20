16ec049be4f6273778b36df1abefaf59cfc4198b
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-03-20 16:07:50 -0300

    Criar API RESTful para Course com Validação Spring Validation

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,9 +1,19 @@
 package com.ead.course.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.ead.course.models.CourseModel;
 
 public interface CourseService {
 
 	public void delete(CourseModel courseModel);
+
+	public CourseModel save(CourseModel courseModel);
+
+	public Optional<CourseModel> findById(UUID courseId);
+
+	public List<CourseModel> findAll();
 	
 }
--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ead.course.services.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import javax.transaction.Transactional;
 
@@ -44,6 +46,21 @@ public class CourseServiceImpl implements CourseService{
 			this.moduleRepository.deleteAll(moduleModelList);
 		}
 		this.courseRepository.delete(courseModel);
+	}
+
+	@Override
+	public CourseModel save(CourseModel courseModel) {
+		return courseRepository.save(courseModel);
+	}
+
+	@Override
+	public Optional<CourseModel> findById(UUID courseId) {
+		return courseRepository.findById(courseId);
+	}
+
+	@Override
+	public List<CourseModel> findAll() {
+		return courseRepository.findAll();
 	}
 	
 }
