3a18654e826383a90eb73f243f98620a9fce0001
Yane Wanderley <yane.rodrigues@ufpe.br>
2024-03-29 02:37:47 -0300

    Comunicação Síncrona - Preparação de Deleção Entre Microservices

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -13,9 +13,11 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.CourseUserRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
@@ -32,6 +34,9 @@ public class CourseServiceImpl implements CourseService{
 	@Autowired
 	LessonRepository lessonRepository;
 	
+	@Autowired
+	CourseUserRepository courseUserRepository;
+	
 	/**
 	 * Modo mais performático (maior desempenho) de deleção, sem delegar ao JPA nem ao Banco de dados.
 	 */
@@ -47,6 +52,10 @@ public class CourseServiceImpl implements CourseService{
 				}
 			}
 			this.moduleRepository.deleteAll(moduleModelList);
+		}
+		List<CourseUserModel> courseUserModelList = courseUserRepository.findAllCourseUserIntoCourse(courseModel.getCourseId());
+		if(!courseUserModelList.isEmpty()) {
+			this.courseUserRepository.deleteAll(courseUserModelList);	
 		}
 		this.courseRepository.delete(courseModel);
 	}
